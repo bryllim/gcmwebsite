2ed2085c8c751d82773ea42c97d089d48d142298
Bryl Lim <bryllim@gmail.com>
2020-04-25 22:02:10 +0800

    changed photo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="WPECR874T96NL">
-        <input type="image" src="https://lh3.googleusercontent.com/proxy/NJmAZebgtnYI6rfYYLUL1O_JQ1AUZzcpB2uemBiHH9B_Rz9oMwChXUIT2jIxp2nng4DIZe3wnEIU-EB7zf8"  name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <input type="image" src="https://i.ya-webdesign.com/images/credit-card-accepted-png.png"  name="submit" alt="PayPal - The safer, easier way to pay online!">
         <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
       </form>
     </div>
